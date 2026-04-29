8e4f975fa60136636b71e6f315f4e1ae907ae30c
jorgegarridot2014-afk <jorgegarridot2014@gmail.com>
2026-04-29 22:02:00 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 const express = require("express");
 const app = express();
 
-let mantenimiento = true;
-
 app.use(express.static(__dirname));
 
-app.get("/estado", (req, res) => {
-  res.json({ mantenimiento });
-});
-
-app.get("/on", (req, res) => {
-  mantenimiento = true;
-  res.send("ON");
-});
-
-app.get("/off", (req, res) => {
-  mantenimiento = false;
-  res.send("OFF");
+app.get("/", (req, res) => {
+  res.send("Servidor funcionando 🔥");
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log("Servidor corriendo");
+  console.log("Servidor corriendo en puerto " + PORT);
 });

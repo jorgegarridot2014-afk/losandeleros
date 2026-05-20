8d6bdc54b4d303e1830806744e9a8cdd45d000a3
jorgegarridot2014-afk <jorgegarridot2014@gmail.com>
2026-05-20 08:34:55 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
-const express = require("express");
-const app = express();
+const mantenimiento = true;
 
 app.get("*", (req,res)=>{
-  res.sendFile(__dirname + "/maintenance.html");
+  if(mantenimiento){
+    return res.sendFile(__dirname + "/maintenance.html");
+  }
+  res.sendFile(__dirname + "/index.html");
 });
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT);

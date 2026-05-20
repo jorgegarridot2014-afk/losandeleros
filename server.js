8a9933f37c726329d4bd2001381daeec106b8746
jorgegarridot2014-afk <jorgegarridot2014@gmail.com>
2026-05-20 08:48:43 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const mantenimiento = true;
+const express = require("express");
+const app = express();
 
-app.get("*", (req,res)=>{
-  if(mantenimiento){
-    return res.sendFile(__dirname + "/maintenance.html");
-  }
-  res.sendFile(__dirname + "/index.html");
+/* 💀 SERVIR ARCHIVOS (si los tienes) */
+app.use(express.static("public"));
+
+/* 💀 RUTA PRINCIPAL */
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+/* 💀 PUERTO RENDER */
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor funcionando en puerto " + PORT);
 });

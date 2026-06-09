08c1153ff971f119894a36d571e40ddaedad2dd8
jorgegarridot2014-afk <jorgegarridot2014@gmail.com>
2026-06-09 21:10:51 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const path = require("path");
 
 const app = express();
 
-/* ================= MANTENIMIENTO SIEMPRE ================= */
-app.use((req, res) => {
-  return res.sendFile(path.join(__dirname, "public", "mantenimiento.html"));
+/* ================= SOLO REFLEJOS ================= */
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "reflejos.html"));
 });
 
 /* ================= START ================= */
 app.listen(process.env.PORT || 3000, () => {
-  console.log("🚧 Modo mantenimiento activo siempre");
+  console.log("🚀 Reflejos activo");
 });
